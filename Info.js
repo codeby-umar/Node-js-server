dc8c9a7ce4bfa6c0cecec84ebef5261ed31fabb7
codeby-umar <codingbyumar@gmail.com>
2026-03-16 09:55:16 +0500

    first commit

---
+++ Info.js
@@ -0,0 +1,79 @@
+// const  Get_Fullinfo = require('./index')
+// Get_Fullinfo("Muhammad Umar" , "Turg'unboyov" , 18 , "Dasturlash" , "Coding")
+
+// const path = require('path');
+
+// const Get_Object = path.parse(__dirname)
+// console.log(Get_Object);
+
+// const Get_FileName = path.parse(__filename)
+// console.log(Get_FileName);
+
+
+// let join = path.join("Muhammad Umar") // Join bizga string element qoshib beradi
+// let kop = path.join("Hello")
+// let full = `${kop} ${join}`
+// console.log(full)   
+
+
+// let baseName = path.basename("hello.world")
+// console.log(baseName)
+
+
+// const resault = path.join("users" , "umar" , "docs" , "file") // path bu fayl qilib beradi yani fayl korinishiga keltiras=di
+// console.log(resault)
+
+
+// const Separate = path.basename("users/umar/text.txt") // basename - bu bir qancha papkalardan faylni ajratib beradi
+// console.log(Separate)
+
+// const tailim = path.basename("users/umar/text.js" , ".js") // basename - bu yani oxirgi faylni extention olib tashlash uchun ham ishlatishimisz mumkin
+// console.log(tailim)
+
+
+// const get_full = path.extname("index.html")
+// console.log(get_full)
+
+
+// const alones = path.resolve("docs" , "app.py")
+// console.log(alones)
+
+
+// const urls = path.normalize("users//hello/desktop//text.py") // notogri yo'llarni tozalab beradi
+// console.log(urls)
+
+
+// const get_Check = path.isAbsolute("/Users/nxt/Desktop/Node js/info.js") // Yol borligini yoki yoqligini tekshirib beradi
+// console.log(get_Check)
+
+
+// const text = path.parse("/Users/nxt/Desktop/Node js/docs/app.py")
+// console.log(text)
+
+
+// const Create_Folder = path.format({
+//     dir : "users/home",
+//     name : "index",
+//     ext : "py"
+// })
+
+// console.log(Create_Folder)
+
+const umar = require('path')
+
+
+// const get_Folders = umar.basename("documents/reports.pdf")
+// console.log(get_Folders)
+
+
+// const hello = umar.extname("/home/user/photo.png")
+// console.log(hello)
+
+
+// const yoli = umar.dirname("/user.desktop/hello.js")
+// console.log(yoli)
+
+
+// const hum = umar.relative("/home/umar/docs" , "/home/umar/images/photo.jpg")
+// console.log(hum)
+
---
+++ index.js
@@ -0,0 +1,22 @@
+// function Get_Name(name , familya){
+//     return(
+//         console.log("Sizning isningiz : " , name , "Sizning famiyaniz :" , familya)
+//     )
+// }
+
+
+// module.exports.endPoind = Get_Name;
+
+
+
+
+// function Get_Fullinfo(name , familya , age , interesting , hobbiy){
+//     return (
+//          console.log("Sizning isningiz : " , name ,
+//           "Sizning faminlaningiz :" , familya , `Sizning yoshingiz : ${age}` , `Sizning qiziqishingiz ${interesting}`,
+//           `Sizning hobbiyingiz : ${hobbiy}`
+//          )
+//     )
+// }
+
+// module.exports = Get_Fullinfo;
